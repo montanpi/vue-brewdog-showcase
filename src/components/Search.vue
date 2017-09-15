719dd7e65d8190d32176d9c9899e26f3825a0071
pmontanari <pmontanari@engagepeople.com>
2017-09-15 12:51:08 +0200

    Initial commit

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -16,21 +16,21 @@
             </div>
         </div>
         <div class="row">
-            <div class="col-sm-4 text-center" v-for="result in results">
-                <div class="card">
-                    <img class="card-img-top img-responsive center-block" src="https://placehold.it/300"
-                         alt="Card image cap">
-                    <div class="card-body">
-                        <h4 class="card-title">{{ result.name}}</h4>
-                        <p class="card-text">ABV: {{ result.abv }}</p>
-                        <p class="card-text">Malts: {{ result.abv }}</p>
-                        <ul>
-                            <li v-for="malt in result.ingredients.malt" class="card-text">{{ malt.name }}</li>
-                        </ul>
-                        <ul>
-                            <li v-for="hop in result.ingredients.hops" class="card-text">{{ hop.name }}</li>
-                        </ul>
-                        <a href="#" class="btn btn-primary">See full description</a>
+            <div class="card col-sm-6 col-md-4 col-lg-3" v-for="result in results">
+                <img class="card-img-top img-responsive center-block" v-bind:src="result.image_url"
+                     alt="Card image cap">
+                <div class="card-body">
+                    <h4 class="card-title">{{ result.name}}</h4>
+                    <p class="card-text">ABV: {{ result.abv }}%</p>
+                    <p class="card-text">Malts: <span v-for="(item, index) in result.ingredients.malt">{{ item.name
+                        }}<span v-if="index != (result.ingredients.malt.length - 1)">, </span></span></p>
+                    <p class="card-text">Hops: <span v-for="(item, index) in result.ingredients.hops">{{ item.name
+                        }}<span v-if="index != (result.ingredients.hops.length - 1)">, </span></span></p>
+                    <div class="btn-group">
+                        <button type="button" class="btn btn-primary">See full description</button>
+                        <button type="button" class="btn btn-primary" data-toggle="tooltip" title="Add to favorites" data-placement="top" @click="toggleFavorite">
+                            <span class="glyphicon glyphicon-star-empty"></span>
+                        </button>
                     </div>
                 </div>
             </div>
@@ -39,27 +39,42 @@
 </template>
 
 <script>
-    import axios from 'axios'
+  import axios from 'axios'
 
-    export default {
-      name: 'search',
-      data () {
-        return {
-          results: []
-        }
+  export default {
+    name: 'search',
+    data () {
+      return {
+        results: []
+      }
+    },
+    methods: {
+      search: function () {
+        axios.get('https://api.punkapi.com/v2/beers?brewed_before=11-2012&abv_gt=6 ')
+          .then(response => {
+            this.results = response.data
+          })
       },
-      methods: {
-        search: function () {
-          axios.get('https://api.punkapi.com/v2/beers/random')
-            .then(response => {
-              this.results = response.data
-              console.log(this.results)
-            })
-        }
+      toggleFavorite: function () {
+        console.log(this.results)
       }
     }
+  }
 </script>
 
-<style>
+<style scoped>
+    .card {
+        padding-right: 40px;
+        padding-left: 40px;
+        padding-top: 10px;
+        padding-bottom: 20px;
+        height: 700px;
+    }
+
+    img {
+        height: 300px;
+    }
+
     /* TODO Search form width */
+    /* TODO Bootstrap style for cards doesn't work (e.g. .card-inverse) */
 </style>
